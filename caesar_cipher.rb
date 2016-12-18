b4ed27b54bceb30f426bd9750894ff640f15ecf6
Stefan L. Barrieres <slb5418@gmail.com>
2016-12-18 17:55:50 -0500

    caesar cipher sinatra

---
+++ caesar_cipher.rb
@@ -0,0 +1,40 @@
+require 'sinatra'
+
+get '/' do 
+	string = params['string'] ? params['string'] : "default"
+	cipher = caesar_cipher(string, 5)
+	erb :index, :locals => {:cipher => cipher}
+end
+
+def caesar_cipher(string, shift)
+	alphabet = ('a'..'z').to_a
+	cipher = ''
+
+	string.each_char do |char|
+		# check if special char
+		if !alphabet.include? char.downcase
+			cipher << char
+			next
+		end
+
+		# check if capital
+		is_upper = true if char == char.upcase
+
+		# find the index and add shift
+		i = alphabet.index(char.downcase).to_i + shift
+
+		# if over 26 take modulus
+		i = i % 26 if i > 26
+ 
+		if is_upper
+			cipher << alphabet[i].upcase 
+		else 
+			cipher << alphabet[i]
+		end
+	end
+	return cipher
+end
+
+# if __FILE__ == $0
+# 	caesar_cipher("What a string!", 5)
+# end
